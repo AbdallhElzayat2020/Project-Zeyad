9dc6bd2f5e2a99e4ce156e373a8df0a3ec6de6e3
AbdallhElzayat2020 <https://github.com/AbdallhElzayat2020/E-commerce-React-App>
2023-07-12 01:03:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// ScrollReveal({
+//     reset: true
+// });
+
+// ScrollReveal({
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200,
+// });
+
+// ScrollReveal().reveal('.logo', {
+//     origin: "top"
+// });
